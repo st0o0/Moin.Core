c814aeedbdfe4e3eb28e7e9701816ef85e08f436
Jan <jan.schloots@bluewin.ch>
2023-11-22 13:25:25 +0100

    add operator

--- src/FicktEuchAllee.Core/Mqtt/Segment.cs
+++ src/FicktEuchAllee.Core/Mqtt/Segment.cs
@@ -8,5 +8,8 @@ public readonly struct Segment(string segment) : IEquatable<Segment?>
     public bool IsSYS => Content.StartsWith('$');
 
     public bool Equals(Segment? other) => Content == other?.Content;
+    public static bool operator ==(Segment left, Segment right) => left.Equals(right);
+    public static bool operator !=(Segment left, Segment right) => !(left == right);
     public override bool Equals(object obj) => obj is Segment segment1 && Equals(segment1);
+    public override int GetHashCode() => Content.GetHashCode();
 }
