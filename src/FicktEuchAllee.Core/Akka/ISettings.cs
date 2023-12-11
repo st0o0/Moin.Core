54ea362a50a91a76fde14f01203772aa22c6b179
Jan Schloots <jan.schloots@advastore.com>
2023-12-11 20:33:11 +0100

    feat: :recycle: Add comments
    +semver:feature

--- src/FicktEuchAllee.Core/Akka/ISettings.cs
+++ src/FicktEuchAllee.Core/Akka/ISettings.cs
@@ -4,24 +4,38 @@ namespace FicktEuchAllee.Core;
 
 /// <summary>
 /// </summary>
-public interface ISettings
-{
+public interface ISettings { }
 
-}
-
+/// <summary>
+/// </summary>
 public class ShardSettings : ISettings
 {
+    /// <summary>
+    /// </summary>
     public int MaxShards { get; init; } = 150;
+
+    /// <summary>
+    /// </summary>
     public bool ShouldPassivateIdleEntities { get; init; } = true;
 
+    /// <summary>
+    /// </summary>
     public TimeSpan? PassivateIdleEntityAfter { get; init; } = null!;
 }
 
+/// <summary>
+/// </summary>
 public class SingletonSettings : ISettings
 {
+    /// <summary>
+    /// </summary>
     public int? BufferSize { get; init; } = null;
 
+    /// <summary>
+    /// </summary>
     public LeaseOptionBase? LeaseImplementation { get; init; } = null;
 
+    /// <summary>
+    /// </summary>
     public TimeSpan? LeaseRetryInterval { get; init; } = null;
 }
