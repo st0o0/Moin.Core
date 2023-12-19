34d3566e4f9c12630e4138f3be0d85c6aa5b1955
Jan <jan.schloots@bluewin.ch>
2023-12-19 19:55:10 +0100

    fix: :bug: new 0.4.0 +semver:feature

--- src/FicktEuchAllee.Core/Mqtt/StringExtensions.cs
+++ src/FicktEuchAllee.Core/Mqtt/StringExtensions.cs
@@ -1,6 +1,12 @@
 ﻿namespace FicktEuchAllee.Core;
 
+/// <summary>
+/// </summary>
 public static class StringExtensions
 {
+    /// <summary>
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <returns></returns>
     public static List<Segment> ToSegments(this string topic) => topic.Split(Matcher.SegmentSeperator).Select(x => new Segment(x)).ToList();
 }
--- src/FicktEuchAllee.Core/Mqtt/Subscription.cs
+++ src/FicktEuchAllee.Core/Mqtt/Subscription.cs
@@ -2,17 +2,40 @@
 
 namespace FicktEuchAllee.Core;
 
+/// <summary>
+/// </summary>
 public interface IMqttMessage
 { }
 
+/// <summary>
+/// </summary>
+/// <param name="topic"></param>
 public abstract class MqttSubscription(string topic)
 {
+    /// <summary>
+    /// </summary>
     public string Topic { get; } = topic;
+
+    /// <summary>
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <param name="payload"></param>
+    /// <returns></returns>
     public abstract Task ExecuteIfMatch(string topic, string payload);
 }
 
+/// <summary>
+/// </summary>
+/// <typeparam name="TMessage"></typeparam>
+/// <param name="topic"></param>
+/// <param name="callback"></param>
 public class MqttSubscription<TMessage>(string topic, Func<TMessage, IEnumerable<string>, Task> callback) : MqttSubscription(topic) where TMessage : IMqttMessage
 {
+    /// <summary>
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <param name="payload"></param>
+    /// <returns></returns>
     public override async Task ExecuteIfMatch(string topic, string payload)
     {
         if (!Matcher.Match(Topic, topic, out var wildcards)) return;
@@ -21,14 +44,26 @@ public class MqttSubscription<TMessage>(string topic, Func<TMessage, IEnumerable
     }
 }
 
+/// <summary>
+/// </summary>
+/// <param name="subscribe"></param>
 public class MqttSubscriptionHandler(Subscribe subscribe)
 {
     private readonly List<MqttSubscription> _subscriptions = [];
     private readonly HashSet<string> _localSubscriber = [];
     private readonly Subscribe _subscribe = subscribe;
 
+    /// <summary>
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <returns></returns>
     public bool HasTopicSubscribed(string topic) => _localSubscriber.Contains(topic);
 
+    /// <summary>
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <param name="payload"></param>
+    /// <returns></returns>
     public async Task HandleMessageAsync(string topic, string payload)
     {
         try
@@ -46,6 +81,12 @@ public class MqttSubscriptionHandler(Subscribe subscribe)
         }
     }
 
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="topic"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
     public bool CreateSubscription<TMessage>(string topic, Func<TMessage, IEnumerable<string>, Task> callback) where TMessage : IMqttMessage
     {
         if (_localSubscriber.Contains(topic)) return false;
