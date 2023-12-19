4a1b11ecbea53ae262d19c92ec96011ef489d8bb
Jan <jan.schloots@bluewin.ch>
2023-12-19 07:05:37 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ src/FicktEuchAllee.Core/Akka/ISettings.cs
@@ -0,0 +1,50 @@
+﻿using Akka.Hosting.Coordination;
+using Akka.Persistence.Hosting;
+
+namespace FicktEuchAllee.Core;
+
+/// <summary>
+/// </summary>
+public interface ISettings { }
+
+/// <summary>
+/// </summary>
+public class ShardSettings : ISettings
+{
+    /// <summary>
+    /// </summary>
+    public int MaxShards { get; init; } = 150;
+
+    /// <summary>
+    /// </summary>
+    public bool ShouldPassivateIdleEntities { get; init; } = true;
+
+    /// <summary>
+    /// </summary>
+    public TimeSpan? PassivateIdleEntityAfter { get; init; } = null!;
+
+    /// <summary>
+    /// </summary>
+    public JournalOptions? JournalOptions { get; init; } = null!;
+    
+    /// <summary>
+    /// </summary>
+    public SnapshotOptions? SnapshotOptions { get; init; } = null!;
+}
+
+/// <summary>
+/// </summary>
+public class SingletonSettings : ISettings
+{
+    /// <summary>
+    /// </summary>
+    public int? BufferSize { get; init; } = null;
+
+    /// <summary>
+    /// </summary>
+    public LeaseOptionBase? LeaseImplementation { get; init; } = null;
+
+    /// <summary>
+    /// </summary>
+    public TimeSpan? LeaseRetryInterval { get; init; } = null;
+}
