61b96186f024d2ba70c4a80c3bbb2d8ce6ccae26
Jan <jan.schloots@bluewin.ch>
2023-12-19 08:05:14 +0100

    more doc +semver:major

--- src/FicktEuchAllee.Core/Akka/MessageExtractor.cs
+++ src/FicktEuchAllee.Core/Akka/MessageExtractor.cs
@@ -3,13 +3,13 @@
 namespace FicktEuchAllee.Core;
 
 /// <summary>
+/// 
 /// </summary>
-/// <remarks>
-/// </remarks>
+/// <param name="maxShards"></param>
 public class MessageExtractor(int maxShards = 50) : HashCodeMessageExtractor(maxShards)
 {
     /// <summary>
     /// </summary>
     public override string EntityId(object message)
-        => message is IShardId shard ? shard.Id : Guid.NewGuid().ToString();
+        => message is IEntityId ntt ? ntt.Id : throw new InvalidOperationException("no entityId");
 }
